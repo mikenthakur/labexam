a5414001551e6873fb2d03cbdade9fc5f84e33a2
vedanthakur <vedanthakur@gmail.com>
2023-08-18 15:23:30 +0545

    add form userinfo

---
+++ inc/userinfo.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+if (isset($_POST["submit"])) {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $nationality = $_POST["nationality"];
+    
+    require_once "dbconfig.php";
+
+    $sql = "INSERT INTO `userinfo`(`name`, `email`, `phone`, `nationality`) VALUES ($name , $email, $phone, $nationality)";
+
+    $result = mysqli_query($conn, $sql);
+
+    if (!$result) {
+        die("Error: couldn't insert the data");
+    } else {
+        echo "Data inserted sucessfully !! ";
+    }
+
+} else {
+    header("location: ../index.php");
+}
+
+?>
---
+++ userinfo.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Blog</title>
+</head>
+<body>
+
+    <main>
+        <h1>Add Blog</h1>
+        <form action="inc/userinfo.inc.php" method="post" enctype="multipart/form-data">
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name" placeholder="Enter name" required><br><br>
+
+            <label for="email">Image</label>
+            <input type="email" name="email" id="email" placeholder="Enter email" required><br><br>
+
+            <label for="phone">Phone</label><br>
+            <input type="tel" name="phone" id="phone" placeholder="Enter phone no." required><br><br>
+
+            <label for="nationality">Nationality</label><br>
+            <input type="text" name="nationality" id="nationality" placeholder="Enter nationality" required><br><br>
+
+            <input type="submit" name="submit" value="Submit">
+
+        </form>
+    </main>
+    <?php include 'footer.php'; ?>
+</body>
+</html>
