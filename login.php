fc476cd124938820a554e05d923d71c5ee491ad7
vedanthakur <vedanthakur@gmail.com>
2023-08-18 15:09:04 +0545

    add login

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log in</title>
+</head>
+<body>
+    <main>
+      <form action="inc/login.inc.php" method="post">
+        <label for="email">Email Address</label>
+        <input type="email" id ="email" name="email" placeholder="Enter email" required><br><br>
+
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" placeholder="Enter passowrd" required><br><br>
+
+        <input type="submit" name="submit" value="Login">
+
+      </form>
+    </main>
+</body>
+</html>
